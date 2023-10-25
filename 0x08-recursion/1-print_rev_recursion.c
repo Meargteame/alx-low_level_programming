deb2db38394e52c42664e7a41a2d834db933d178
meargteame <mearegteame99995555@gmail.com>
2023-10-25 15:10:18 +0300

    this is task 01 recursion project

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,11 +3,13 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _put_rev_recursion(char *s)
-	{
-		if (*s)
+void _print_rev_recursion(char *s)
+{
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	}
+}
+
+
