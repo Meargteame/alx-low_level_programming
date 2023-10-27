cb31243a4da8da1c96d5844647ae81746ab0c884
meargteame <mearegteame99995555@gmail.com>
2023-10-28 00:33:49 +0300

    this is a commit

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-ar -rc liball.a *.o
-ranlib liball.a 
+
+# Compile each .c file into .o object files
+gcc -c *.c
+
+# Create the static library liball.a from the object files
+ar rcs liball.a *.o
+
+# Generate an index for the static library
+ranlib liball.a
+
+# Clean up the object files
+rm *.o 
