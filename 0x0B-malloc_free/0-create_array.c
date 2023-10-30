c7aef0a19514f519efc0c12198b0dbc76c937c22
meargteame <mearegteame99995555@gmail.com>
2023-10-31 01:00:47 +0300

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *create_array-creates an array
+ * @c - character to store an array
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	if(size ==0)
+	{
+		return (NULL);
+
+	arr =  malloc (sizeof(char)*size);
+        if (arr
+}
