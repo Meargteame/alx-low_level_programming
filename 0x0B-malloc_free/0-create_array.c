c351b38daf5c0e85e0808cfce7bda2c64b60a48e
meargteame <mearegteame99995555@gmail.com>
2023-10-31 08:11:05 +0300

    this is a commit for the first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,22 +14,14 @@ char *create_array(unsigned int size, char c)
 	if(size ==0)
 	{
 		return (NULL);
-
-	arr =  malloc (sizeof(char)*size);
+        arr =  malloc (sizeof(char)*size);
         if (arr === NULL)
-	
 	{
 		return (NULL);
         }
-
 	for(int i =0; i< size ;i++)
-	
 	{
 	   arr[i] = c;
-
-          
 	} 
        return (arr);
-
-
 }
