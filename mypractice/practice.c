60d2b43a691c7b47360c65c5ac01e4b5ac4b39e2
meargteame <mearegteame99995555@gmail.com>
2023-11-25 10:48:10 +0300

    this is C preproccesors Project : 4 Tasks

---
+++ mypractice/practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * this is nested-for loop program 
+ * maina(0) - is the the main function
+ * return (0)- return 0
+ */
+int main(void){
+
+	for (int j=0;j<=10;j++)
+
+	{
+	
+		for(int i=0;i<=10;i++)
+
+		{
+
+			printf("*");
+		}
+         
+		printf("\n");
+
+	}
+
+	return (0);
+}
