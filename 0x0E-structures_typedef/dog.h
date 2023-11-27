f76604cb3078d41d7e406ab09a4fd55eec0a38ad
meargteame <mearegteame99995555@gmail.com>
2023-11-27 15:20:12 +0300

    this sicommit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - dog's structure 
- * @name - name of the dog
- * @age :age of the dog
- * @owner - name of the owner
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
 
-
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
